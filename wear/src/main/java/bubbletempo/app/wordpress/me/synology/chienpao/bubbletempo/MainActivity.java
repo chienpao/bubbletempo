f064a9e89bfb0f0cb11f88b59cfb16f63678cde8
Pao <designer.pao@gmail.com>
2014-12-27 23:16:36 +0800

    Init Project

---
+++ wear/src/main/java/bubbletempo/app/wordpress/me/synology/chienpao/bubbletempo/MainActivity.java
@@ -0,0 +1,35 @@
+package bubbletempo.app.wordpress.me.synology.chienpao.bubbletempo;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.wearable.view.WatchViewStub;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+
+    private Button button;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
+        stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
+            @Override
+            public void onLayoutInflated(WatchViewStub stub) {
+                button = (Button) stub.findViewById(R.id.button);
+            }
+        });
+    }
+
+    public void onButtonFunc(View view){
+        Log.i("Pao", "success");
+    }
+
+    public void onStartFunc(View view){
+
+    }
+}
